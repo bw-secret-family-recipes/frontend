2442a7e3bcf1f08b2186842f00145eb3380b2f45
foobar404 <austinthemichaud@gmail.com>
2019-10-21 18:03:10 -0400

    build out utils build out routes
    Co-authored-by: Matt G <matt@dustcloudlabs.com>
    Co-authored-by: Daniel Briksza <dbriksza@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import { Route } from "react-router-dom"
+import { Route, Switch, Redirect } from "react-router-dom"
 
 import Nav from './components/Nav';
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import RecipeList from "./components/RecipeList";
+import NewRecipe from "./components/NewRecipeForm";
+
+import { reducer, Context } from "utils";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer);
   return (
-    <main>
-    
-      <header className="App-header">
-        <Nav />
-      </header>
-    
-      <Route></Route>// Dashboard /dashboard
-      <Route></Route>// Login /login
-      <Route></Route>// Signup /signup
-      <Route></Route>// NewRecipeForm /create
-      <Route></Route>// Edit?
-    </main>
+    <Context.Provider value={{ state, dispatch }}>
+      <main>
+
+        <header className="App-header">
+          <Nav />
+        </header>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <PrivateRoute path="/dashboard" component={RecipeList} />
+          <PrivateRoute path="/new-recipe" component={NewRecipe} />
+          <Redirect from="/login" to="/dashboard" />
+        </Switch>
+
+        {/* <Route />// Dashboard /dashboard
+        <Route></Route>// Login /login
+        <Route></Route>// Signup /signup
+        <Route></Route>// NewRecipeForm /create
+        <Route></Route>// Edit? */}
+      </main>
+    </Context.Provider>
   );
 }
 
