e34b5d60cb74ee3826f759884d3391b1035d16e5
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-10-21 14:29:58 -0400

    install w/ yarn, removed react icons, started Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react-router-dom';
+
+
+//nav needs dashboard, receipe, search 
+const Nav = () =>{
+
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+export default Nav
