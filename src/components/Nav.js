0483596a6981bd5aba52402de1665caea8ff9933
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-10-21 18:07:36 -0400

    rework nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
+import logo from './images/logo.png';
 
-
-
-const NavUl = styled.ul`
-    display: flex;
-    width: 800px;
-    justify-content: space-between;
-    background: #3f043c;
-    padding: 1rem 2rem;
-    border-radius: 0px 0 5px 5px;
-`;
-const NavLi = styled.li`
-    display:flex;
-    text-decoration: none;
-    
-`;
-const StyledLink = styled(Link)`
+const NavWrap = styled.nav`
     font-family: 'Poppins', sans-serif;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem;
+    background: #3f043c;
+`;
+
+const ImgBox = styled.img`
+    width: 200px;
+
+`;
+
+const LinkWrap = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 60%;
+`;
+
+const StyledLink = styled(Link)`
     text-decoration: none;
     color: #8ec63f;
     font-size: 2rem;
@@ -32,24 +37,17 @@ const StyledLink = styled(Link)`
 const Nav = () =>{
 
     return (
-        <div>
-            <nav>
-                
-                <NavUl>
-                    <NavLi><StyledLink to="">
-                    Dashboard</StyledLink></NavLi>
-
-                    <NavLi><StyledLink to="">
-                    Receipe</StyledLink></NavLi>
-
-                    <NavLi><StyledLink>
-                    Search</StyledLink></NavLi>
-
-                    <NavLi><StyledLink>
-                    Sign Out</StyledLink></NavLi>
-                </NavUl>
-            </nav>
-        </div>
+        <NavWrap>
+            <div>
+                <ImgBox src={logo} alt='Secrect Family Recipes' />
+            </div>
+            <LinkWrap>
+                <StyledLink to="">Dashboard </StyledLink>
+                <StyledLink to="">Receipe   </StyledLink>
+                <StyledLink to="">Search    </StyledLink>
+                <StyledLink to="">Sign Out  </StyledLink>
+            </LinkWrap>
+        </NavWrap>
     );
 }
 export default Nav
