edfc5087065f375c035e9af2d2537c1102a9ccb6
Schrese <53787633+Schrese@users.noreply.github.com>
2019-10-21 17:42:55 -0400

    working on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+import logo from './images/logo.png';
+
+const FootContainer = styled.div`
+    display: flex;
+    width: 90%;
+    justify-content: center;
+    background: #8A00D4;
+    flex-direction: column;
+    color: white;
+`;
+
+const Address = styled.p`
+    display: flex;
+    justify-content: center;
+`;
+const Image = styled.img`
+    display: flex;
+    justify-content: center;
+`
 
 const Footer = () => {
 
     return (
-        <div className = 'footer-container'>
-            <h4>Secret Family Recipe</h4>
-            <p>Built By: Daisy Mesa, Himmat Gill, Daniel Briksza, Austin Michaud, Mason Roberts, Schrese Holloway, Constance Beebe, Devanee Reid </p>
-        </div>
+        <FootContainer>
+            <Address>
+                <p>Secret Family Recipes</p>
+                <p>1054 Somewhere Street</p>
+                <p>Whoville, TN 086520 USA</p>
+                <p>800.222.2222</p>
+            </Address>
+            <Image>
+                <img src = {logo} alt = 'logo'></img>
+            </Image>
+        </FootContainer>
     )
 }
+
+export default Footer;
