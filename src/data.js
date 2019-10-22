649a2a522768352036e552b7cd24e16d15258384
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-10-22 13:36:16 -0400

    Data.js with 2 recipe info started.

---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const recipe = [
+    {
+    "id": 1,
+    "user_id": 5,
+    "recipe_name": "Moms Spaghetti",
+    "source": "string",
+    "categories": [
+       "Dinner",
+       "Beef"
+    ],
+    "ingredients": [
+       "Spaghetti pasta serving 4",
+       "1 can Spaghetti Sauce",
+       "1 can Crushed Tomatoes",
+       "1 healthy tablespoon italian seasoning",
+       "1 healthy teaspoon worchestershire sauce",
+       "1.5 lb ground beef",
+       "1 medium onion",
+       "1 medium green pepper",
+       "1 heathly tablespoon garlic",
+    ],            
+    "recipe_instructions": "Chop onion and green pepper, Begin to brown ground beef, add onion and pepper once beef is nearly brown, drain excess grease, add crushed tomatoes, sauce, garlic, seasoning, simmer to thicken, the longer you simmer the better. Boil pasta about 7 min, serve sauce over pasta.",
+},
+{
+    "id": 2,
+    "user_id": 5,
+    "recipe_name": "Grilled Cheese",
+    "source": "string",
+    "categories": [
+       "Lunch",
+       "Dairy"
+    ],
+    "ingredients": [
+       "American Cheese, Slices",
+       "Honey Wheat Bread",
+       "Butter"
+    ],            
+    "recipe_instructions": "Heat small or medium frying pan on medium to medium low heat, melt 1/4 tablespoon butter, construct cheese sandwich, place the sandwich in the butter and brown / toast sandwich side, flip sandwich and add more butter. do not burn butter, it will stink and make sandwich taste bad",
+}
+]
