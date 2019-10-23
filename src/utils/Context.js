3b438c2c46a59192b15561865a483fac25fe503e
mxxt1 <matt@dustcloudlabs.com>
2019-10-23 09:58:25 -0400

    templating Context

--- src/utils/Context.js
+++ src/utils/Context.js
@@ -1,5 +1,17 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
 const Context = createContext({});
+
+// export const ContextProvider = () => {
+
+//     const [userData, setUserData] = useState({
+//         add a useLocalStorage hook, set this up using useLocalStorage
+
+//         Data template here, local storage items here
+
+
+//     });
+// };
+
 
 export default Context ;
